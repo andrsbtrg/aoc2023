938be1fc7cb09f3146247be7126a1631dc77eed0
Andres <andrsbtrg@gmail.com>
2023-12-12 22:28:39 +0100

    Day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    let total: i32 = part_1(&input);
+    let total: i32 = part_2(&input);
 
     println!("total: {total}");
 }
@@ -8,6 +8,56 @@ fn main() {
 const R_MAX: i32 = 12;
 const G_MAX: i32 = 13;
 const B_MAX: i32 = 14;
+
+#[derive(Debug)]
+struct Game {
+    red: Vec<i32>,
+    green: Vec<i32>,
+    blue: Vec<i32>,
+}
+impl Game {
+    fn new(subsets: Vec<&str>) -> Self {
+        let mut game = Game {
+            red: Vec::new(),
+            green: Vec::new(),
+            blue: Vec::new(),
+        };
+
+        for subset in subsets {
+            let records: Vec<&str> = subset.split(", ").collect();
+            records.iter().for_each(|r| {
+                let opt: Vec<&str> = r.split(" ").collect();
+                let n: i32 = opt[0].parse().unwrap();
+
+                match opt[1] {
+                    "blue" => game.blue.push(n),
+                    "red" => game.red.push(n),
+                    "green" => game.green.push(n),
+                    _ => panic!(),
+                };
+            });
+        }
+
+        game
+    }
+}
+
+fn part_2(input: &str) -> i32 {
+    let mut sum_powers = 0;
+    input.lines().for_each(|line| {
+        let v: Vec<&str> = line.split(": ").collect();
+
+        // let game_id: i32 = get_id(v[0]);
+        let subsets: Vec<&str> = v[1].split("; ").collect();
+
+        let game = Game::new(subsets);
+        let min_red = game.red.iter().max().unwrap();
+        let min_blue = game.blue.iter().max().unwrap();
+        let min_green = game.green.iter().max().unwrap();
+        sum_powers += (min_red * min_blue * min_green);
+    });
+    sum_powers
+}
 
 fn part_1(input: &str) -> i32 {
     let mut sum_ids = 0;
