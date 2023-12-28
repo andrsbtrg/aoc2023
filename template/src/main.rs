bed907fb0fd1a7d120f733157ca358b4c2e91067
Andres <andrsbtrg@gmail.com>
2023-12-28 17:07:36 +0100

    Add template

---
+++ template/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let input = std::fs::read_to_string("input.txt").unwrap();
+
+    let value: u32 = part_2(&input);
+
+    println!("total: {}", value);
+}
+
+fn part_2(_input: &str) -> u32 {
+    todo!()
+}
+
+fn _part_1(_input: &str) -> u32 {
+    todo!()
+}
